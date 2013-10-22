2c565f0313a50cd40c44e04769c7aa00a16cdd80
Viriya Ratanasangpunth <ratanasv@eecs.oregonstate.edu>
2013-10-21 18:36:05 -0700

    - update cmakefile to accommodate addition lib directories in Xcode - assert that we can open shp files.

--- test/SHP3DStartupTest.cpp
+++ test/SHP3DStartupTest.cpp
@@ -3,14 +3,21 @@
 #include <vistas/vistas.h>
 #include <boost/tr1/memory.hpp>
 #include <SHP3D.h>
+#include <Envision.h>
 
 using std::tr1::shared_ptr;
 
 CPPUNIT_TEST_SUITE_REGISTRATION( SHP3DStartupTest );
-
+const VI_String DATA_PREFIX = VI_String("/Users/ratanasv/Documents/data/");
 
 void SHP3DStartupTest::testAll()
 {
     shared_ptr<SHP3D> shp3dPlugin(new SHP3D());
+    shared_ptr<EnvisionDataPlugin> envisionPlugin(new EnvisionDataPlugin());
+    const VI_Path pathToShp = VI_Path(DATA_PREFIX + VI_String("eastern_oregon/idu3D.shp"));
+    CPPUNIT_ASSERT(pathToShp.Exists());
+    envisionPlugin->Set(pathToShp);
+    CPPUNIT_ASSERT_EQUAL(8616, envisionPlugin->GetNumShapes());
+    
 }
 
